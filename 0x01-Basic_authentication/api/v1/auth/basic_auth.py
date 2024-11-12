625a0d33810de69ac9695927bf2a595c6256c3bd
Nsikak-mbre <tm260715@gmail.com>
2024-11-12 18:18:49 +0100

    task_8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode base64 authorization header
+        """
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')).decode('utf-8')
+        except Exception:
+            return None
