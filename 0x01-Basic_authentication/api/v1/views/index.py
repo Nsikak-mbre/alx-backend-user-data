0c27ae4c5010dae54d1b786b8c594ed7ee1f86ac
Nsikak-mbre <tm260715@gmail.com>
2024-11-12 03:44:03 +0100

    api

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def test_unauthorized():
+    """Endpoint to test 401 error handler."""
+    abort(401)
