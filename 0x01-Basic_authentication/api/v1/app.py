bdc5eddb31c5cdeea119dbced79d04fa3a8a09df
Nsikak-mbre <tm260715@gmail.com>
2024-11-12 03:55:47 +0100

    task_2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,6 +27,12 @@ def handle_unauthorized(error):
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def handle_forbidden(error):
+    """Return JSON response for 403 Forbidden error."""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
