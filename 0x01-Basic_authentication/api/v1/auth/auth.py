1029fcff2ea538a78106a6b36cf46264762969f3
Nsikak-mbre <tm260715@gmail.com>
2024-11-12 04:52:24 +0100

    task_4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Ensure path ends with /
+        if not path.endswith('/'):
+            path += '/'
+
+        # Ensure path is not in excluded_paths
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self) -> str:
         """Authorization header
