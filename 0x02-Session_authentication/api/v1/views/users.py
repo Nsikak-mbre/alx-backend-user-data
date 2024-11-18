5d04c6dad718972da92ea5c0a2895d94fa416fa7
Nsikak-mbre <tm260715@gmail.com>
2024-11-18 02:40:17 +0100

    0_task

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -26,7 +26,7 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
     """
     # gets a user by id
-    if  user_id == 'me':
+    if user_id == 'me':
         if request.currrent_user is None:
             abort(404)
         else:
@@ -129,11 +129,11 @@ def update_user(user_id: str = None) -> str:
     return jsonify(user.to_json()), 200
 
 # api/v1/views/users.py
+
+
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def get_authenticated_user():
     """ Retrieve authenticated user """
     if request.current_user is None:
         abort(404)
     return jsonify(request.current_user.to_json())
-
-
