3a8b50a37546128b134a304ad5806e397bacde20
Nsikak-mbre <tm260715@gmail.com>
2024-11-20 02:47:01 +0100

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,9 +2,10 @@
 """
 User class
 """
-from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy.ext.declarative import declarative_base
 from typing import Optional
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -14,8 +15,9 @@ class User(Base.Model):
     User class
     """
     __tablename__: str = 'users'
-    id = Base.Column(Base.Integer, primary_key=True)
-    email = Base.Column(Base.String(250), nullable=False)
-    hashed_password = Base.Column(Base.String(250), nullable=False)
-    session_id = Base.Column(Base.String(250), nullable=True)
-    reset_token = Base.Column(Base.String(250), nullable=True)
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
