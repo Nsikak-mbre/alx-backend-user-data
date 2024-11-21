383c05c2348e83652a4b79fbfc9d8eca34bd57c5
Nsikak-mbre <tm260715@gmail.com>
2024-11-21 22:53:38 +0100

    auth.py_token_reset

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -116,7 +116,7 @@ def profile():
 
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
-def get_reset_password_token():
+def get_reset_password_token() -> str:
     """
     Get a reset password token for a user
 
