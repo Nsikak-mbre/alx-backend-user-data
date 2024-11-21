4438a6b9c28c85aaa380493f5fabe277a777be16
Nsikak-mbre <tm260715@gmail.com>
2024-11-21 22:52:02 +0100

    auth.py_token_reset

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -141,7 +141,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError
+            raise ValueError("Email not found")
         token = str(uuid.uuid4())
         self._db.update_user(user.id, reset_token=token)
         return token
