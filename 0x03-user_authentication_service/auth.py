487fa21af976c37e4d149b01d031e4504ad03374
Nsikak-mbre <tm260715@gmail.com>
2024-11-21 01:20:23 +0100

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,6 +18,7 @@ def _hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
 
+
 def _generate_uuid() -> str:
     """
     Generate a UUID
