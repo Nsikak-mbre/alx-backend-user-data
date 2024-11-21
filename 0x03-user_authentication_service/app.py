7af50c56ed424b0d3108d3d6724eb94c63e54328
Nsikak-mbre <tm260715@gmail.com>
2024-11-21 14:04:46 +0100

    auth.py_session_created_on_login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -25,6 +25,40 @@ def users():
         return jsonify({'email': user.email, 'message': 'user created'})
     except ValueError as err:
         return jsonify({'message': "email already registerd"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """
+    Log in a user and create a session.
+
+    Request:
+    - Form data containing "email" and "password"
+
+    Response:
+    - On success, set a "session_id" cookie and return a JSON payload.
+    - On failure, abort with a 401 HTTP status.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # validate form data
+    if not email or not password:
+        abort(401)
+
+    # validate login
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    # create session
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    # create response
+    response = make_response(jsonify({'email': email, 'message': 'logged in'}))
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
